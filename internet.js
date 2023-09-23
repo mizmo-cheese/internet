38154cc0cac22654f3f70793012a77bc723b8939
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-09-23 16:25:01 +0500

    Update internet.js

--- internet.js
+++ internet.js
@@ -1,7 +1,7 @@
-var inet = __.Browser().link("https://www.google.com/search?igu=1").width("100%").height("100%");
-var Internet = new __.Window({
-  "id": "net",
-  "snapbox": true,
-  "closable": true
-})
-var internetui = new __.UI(Internet, inet);
+var Internet = __.Browser().link("https://www.google.com/search?igu=1").width("100%").height("100%"); // Браузер
+var InternetWindow = new __.Window({ // Окно с браузером
+  "id": "net", // Идентификатор
+  "snapbox": true, // Кнопка развернуть окно
+  "closable": true // Кнопка закрыть окно
+});
+__.UI(InternetWindow, Internet); // Добавить браузер в окно
