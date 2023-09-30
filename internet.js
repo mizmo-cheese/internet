8168dd131adb8db8e267f23d74e941f4d76daf79
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-09-30 18:35:53 +0500

    Update internet.js

--- internet.js
+++ internet.js
@@ -1,20 +1,21 @@
 // Browser - Браузер "Интернет"
 var internet = __.Browser().link("https://www.google.com/search?igu=1").width("100%").height("600");
 var InternetWindow = new __.Window({
-	"id":"net",
+  "id":"net",
   "snapbox": true,
-	"closable":true,
-  "height": 'calc(75% - 31px)'
-})
+  "closable":true,
+  "height": "calc(75% - 31px)"
+});
+
 // Кнопка открытия страницы, указанной в link_address
-var link_opener = __.Button().x(200).y(600)
+var link_opener = __.Button().x(200).y(600);
 // Адресная строка
-var link_address = __.AdvancedInput().x(100).y(600)
+var link_address = __.AdvancedInput().x(100).y(600);
 
 
 // Функция перехода на страницу, указанную в link_address
 function GoToLink() {
-  Onet.link(link_address.text())
+  internet.link(link_address.text());
 }
 
 // Привязка функции к кнопке link_opener
