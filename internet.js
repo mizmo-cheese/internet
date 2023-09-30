2a7e0c3ada043f5d4a80fba0d60b1c2a713a6e65
mizmo <79184880+mizmo-cheese@users.noreply.github.com>
2023-09-30 20:28:08 +0700

    Update internet.js

--- internet.js
+++ internet.js
@@ -1,7 +1,23 @@
-var Internet = __.Browser().link("https://www.google.com/search?igu=1").width("100%").height("100%"); // Браузер
-var InternetWindow = new __.Window({ // Окно с браузером
-  "id": "net", // Идентификатор
-  "snapbox": true, // Кнопка развернуть окно
-  "closable": true // Кнопка закрыть окно
-});
-__.UI(InternetWindow, Internet); // Добавить браузер в окно
+// Browser - Браузер "Интернет"
+var internet = __.Browser().link("https://www.google.com/search?igu=1").width("100%").height("600");
+var InternetWindow = new __.Window({
+	"id":"net",
+  "snapbox": true,
+	"closable":true,
+  "height": 'calc(75% - 31px)'
+})
+// Кнопка открытия страницы, указанной в link_address
+var link_opener = __.Button().x(200).y(600)
+// Адресная строка
+var link_address = __.AdvancedInput().x(100).y(600)
+
+
+// Функция перехода на страницу, указанную в link_address
+function GoToLink() {
+  Onet.link(link_address.text())
+}
+
+// Привязка функции к кнопке link_opener
+link_opener.on("click", GoToLink);
+
+new __.UI(InternetWindow, link_address, internet, link_opener);
